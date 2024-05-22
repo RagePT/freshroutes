b341b5c23d182fc7a091cec5952af8bc2c0e9681
Rage <brothers.craft.td0vader@gmail.com>
2024-05-22 16:53:59 +0100

    Fixed Login system

--- POO_SI/src/projeto/Gestor.java
+++ POO_SI/src/projeto/Gestor.java
@@ -58,81 +58,70 @@ public class Gestor {
      * Metodo para criar perfil
      */
 	public void CriarPerfil() {
-		System.out.println("Deseja criar perfil de:\n1. Administrador\n2. Utilizador");
-		String resposta = sc.nextLine();
-		String user = "";
-		String usertemp;
-		String pass;
-		int num;
-		switch (resposta) {
-		case "1":
-			System.out.println("Qual é o Username?"); // A IMPLEMENTAR : NAO permitir 2 usernames iguais
-			usertemp = sc.nextLine();
-			if (Users.size() == 0) {
-				user = usertemp;
-			} else {
-				for (int i=0;i<Users.size();i++) {
-					if (Users.get(i).getUsername().equals(usertemp)) {
-						System.out.println("Utilizador já existe!");
-					} else {
-						user = usertemp;
-					}
-				}
-					
-			}
-			if (user.equals("")) {
-				break;
-			}
-			System.out.println("Qual é a password?");
-			pass = sc.nextLine();
-			System.out.println("Qual é o numero de telemovel?");
-			num = sc.nextInt();
-			Admin adminuser = new Admin(user,pass,num);
-			int size = Users.size();
-			Users.add(size, adminuser);
-			
-		break;
-		case "2":
-			System.out.println("Qual é o Username?"); // A IMPLEMENTAR : NAO permitir 2 usernames iguais
-			usertemp = sc.nextLine();
-			if (Users.size() == 0) {
-				user = usertemp;
-			} else {
-				for (int i=0;i<Users.size();i++) {
-					if (Users.get(i).getUsername().equals(usertemp)) {
-						System.out.println("Utilizador já existe!");
-					} else {
-						user = usertemp;
-					}
-				}
-					
-			}
-			if (user.equals("")) {
-				break;
-			}
-			System.out.println("Qual é a password?");
-			pass = sc.nextLine();
-			System.out.println("Qual é o numero de telemovel?");
-			num = sc.nextInt();
-			System.out.println("Qual é a sua idade?");
-			int age = sc.nextInt();
-			String def;
-			do {
-				System.out.println("Tem alguma deficiencia?(S/N)");
-				def = sc.nextLine();	
-			}while(!def.equals("S") && !def.equals("N"));
-			boolean deficiencia;
-			if (def.equals("S")) {
-				deficiencia = true;
-			}else deficiencia = false;
-			
-			Admin useruser = new User(user,pass,num,age,deficiencia);
-			int size1 = Users.size();
-			Users.add(size1, useruser);
-		break;
-		default:
-			System.out.println("Opção Inválida.");
-		}
+	    System.out.println("Deseja criar perfil de:\n1. Administrador\n2. Utilizador");
+	    String resposta = sc.nextLine();
+	    String user = "";
+	    String usertemp;
+	    String pass;
+	    int num;
+	    switch (resposta) {
+	        case "1":
+	            System.out.println("Qual é o Username?");
+	            usertemp = sc.nextLine();
+	            boolean userExists = false;
+	            for (int i = 0; i < Users.size(); i++) {
+	                if (Users.get(i).getUsername().equals(usertemp)) {
+	                    System.out.println("Utilizador já existe!");
+	                    userExists = true;
+	                    break;
+	                }
+	            }
+	            if (!userExists) {
+	                user = usertemp;
+	                System.out.println("Qual é a password?");
+	                pass = sc.nextLine();
+	                System.out.println("Qual é o numero de telemovel?");
+	                num = sc.nextInt();
+	                sc.nextLine();
+	                Admin adminuser = new Admin(user, pass, num);
+	                Users.add(adminuser);
+	            }
+	            break;
+	        case "2":
+	            System.out.println("Qual é o Username?");
+	            usertemp = sc.nextLine();
+	            userExists = false;
+	            for (int i = 0; i < Users.size(); i++) {
+	                if (Users.get(i).getUsername().equals(usertemp)) {
+	                    System.out.println("Utilizador já existe!");
+	                    userExists = true;
+	                    break;
+	                }
+	            }
+	            if (!userExists) {
+	                user = usertemp;
+	                System.out.println("Qual é a password?");
+	                pass = sc.nextLine();
+	                System.out.println("Qual é o numero de telemovel?");
+	                num = sc.nextInt();
+	                sc.nextLine();
+	                System.out.println("Qual é a sua idade?");
+	                int age = sc.nextInt();
+	                sc.nextLine();
+	                String def;
+	                do {
+	                    System.out.println("Tem alguma deficiencia?(S/N)");
+	                    def = sc.nextLine();
+	                } while (!def.equals("S") && !def.equals("N"));
+	                boolean deficiencia = def.equals("S");
+	                Admin useruser = new User(user, pass, num, age, deficiencia);
+	                Users.add(useruser);
+	            }
+	            break;
+	        default:
+	            System.out.println("Opção inválida.");
+	            break;
+	    }
 	}
 /**
  * metodo login de user
--- POO_SI/src/projeto/Main.java
+++ POO_SI/src/projeto/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 
 	/**
-	 * dsakdasodasldalla
+	 * dsakdasodasldall
 	 */
 	public static void main(String[] args) {
 		
